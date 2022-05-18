1542ab23f4338775f5eefcd75d872b53f1094b47
yannickdhondt <yannickdhondt@hotmail.com>
2022-05-18 00:43:54 -0700

    fixing add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,14 +11,14 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // Using + operator to type cast variables as integers in order to prevent string concat
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
   };
 
   // Determine the operation
-
   if (! req.query.operation) {
     throw new Error("Unspecified operation");
   }
